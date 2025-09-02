71ac20c68715268a7a00716640f419ba8007f069
thomas <thomasniu@gmail.com>
2025-09-02 13:38:05 +0800

    Implement movie search with OMDB API and clean up index rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const tempMovieData = [
     {
@@ -50,9 +50,24 @@ const tempWatchedData = [
 const average = (arr) =>
     arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = '6509ef70'
+
 export default function App() {
-    const [movies, setMovies] = useState(tempMovieData);
-    const [watched, setWatched] = useState(tempWatchedData);
+    const [movies, setMovies] = useState([]);
+    const [watched, setWatched] = useState([]);
+    const query = 'interstellar'
+
+    useEffect(function () {
+        async function fetchMovies() {
+            const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`)
+            const data = await res.json()
+            setMovies(data.Search)
+            console.log(data.Search)
+        }
+
+        fetchMovies()
+    }, []);
+
 
     return (
         <>
--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-import StarRating from "./StarRating";
+import './index.css';
+import App from './App';
 
-function Test() {
-    const [movieRating, setMovieRating] = useState(0)
-
-    return (
-        <div>
-            <StarRating color='blue' maxRating={10} onSetRating={setMovieRating}/>
-            <p>This movie was rated {movieRating} stars</p>
-        </div>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        {/*<App/>*/}
-        <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
-        <StarRating size={24} color="red" className='test' defaultRating={2}/>
-        <Test/>
+        <App/>
     </React.StrictMode>
 );
